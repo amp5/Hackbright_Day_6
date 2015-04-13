cd326c549b4bc6eebf1cc2ef2c6f7c7dab5c56e1
Hackbright Student <info@hackbrightacademy.com>
2015-04-13 12:55:15 -0700

    created first for loop in function to compile dictionary

--- restaurant-ratings.py
+++ restaurant-ratings.py
@@ -1,1 +1,12 @@
 # your code goes here
+def rest_ratings(filepath):
+    open_file = open(filepath)
+    restaurant_dict = {}
+    for line in open_file:
+        line_list = line.rstrip().split(":")
+        restaurant_dict[line_list[0]] = line_list[1]
+
+
+    print restaurant_dict
+
+rest_ratings("scores.txt")

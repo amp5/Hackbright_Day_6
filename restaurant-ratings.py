c06c637648ef2458e7d7e2addeb82d175391e062
Hackbright Student <info@hackbrightacademy.com>
2015-04-13 13:06:31 -0700

    sorted keys

--- restaurant-ratings.py
+++ restaurant-ratings.py
@@ -2,11 +2,22 @@
 def rest_ratings(filepath):
     open_file = open(filepath)
     restaurant_dict = {}
+
     for line in open_file:
         line_list = line.rstrip().split(":")
         restaurant_dict[line_list[0]] = line_list[1]
 
 
-    print restaurant_dict
+    # test passed -- print restaurant_dict
+
+    #sorted_list = restaurant_dict.keys()
+    #sorted_list.sort()
+
+    #print sorted_list
+    
+    new_sorted_list = sorted(restaurant_dict.keys())
+
+    print new_sorted_list
+
 
 rest_ratings("scores.txt")
